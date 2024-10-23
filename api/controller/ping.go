1cf1ef5cae1b0d1eb6a24220caefc366c8e222e3
Pankaj Pawar] <pankaj91as@gmail.com>
2024-10-23 15:40:40 +0530

    Change response & set app version variable in .env

--- api/controller/ping.go
+++ api/controller/ping.go
@@ -3,9 +3,10 @@ package controller
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func (RC *RestController) PingHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "- Test Commit 7")
+	fmt.Fprintf(w, "- Application Version "+os.Getenv("APP_VERSION"))
 }
